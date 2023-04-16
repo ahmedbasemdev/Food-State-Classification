6cd98bf6a06a7469c80922b0b07c129c0b951347
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2023-04-16 02:10:29 +0200

    App.py

--- app.py
+++ app.py
@@ -7,31 +7,31 @@ import os
 from tqdm import tqdm
 import json
 
-# model = load_model('model.h5',compile=False)
-#
-# optimizer = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
-# model.compile(loss = 'sparse_categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
-#
-#
-# def predict(img_path):
-#     img = cv2.imread(img_path)
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     img = cv2.resize(img, (utily.IMG_SIZE, utily.IMG_SIZE))
-#     img = np.array(img).reshape(-1, utily.IMG_SIZE, utily.IMG_SIZE, 3) / 255.0
-#     result = model.predict(img)
-#     return utily.map_output[np.argmax(result)]
-#
-#
-# outputs = {}
-# test_dir = "test/anonymous"
-# for image in tqdm(os.listdir(test_dir)):
-#     full_path = os.path.join(test_dir,image)
-#     outputs[image] = predict(full_path)
-#
-# with open("predictions.json", "w") as outfile:
-#     json.dump(outputs, outfile)
-#
-#
+model = load_model('model.h5',compile=False)
+
+optimizer = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
+
+
+def predict(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (utily.IMG_SIZE, utily.IMG_SIZE))
+    img = np.array(img).reshape(-1, utily.IMG_SIZE, utily.IMG_SIZE, 3) / 255.0
+    result = model.predict(img)
+    return utily.map_output[np.argmax(result)]
+
+
+outputs = {}
+test_dir = "test/anonymous"
+for image in tqdm(os.listdir(test_dir)):
+    full_path = os.path.join(test_dir,image)
+    outputs[image] = predict(full_path)
+
+with open("predictions.json", "w") as outfile:
+    json.dump(outputs, outfile)
+
+
 truth = open('ground_truth.json')
 truth = json.load(truth)
 
